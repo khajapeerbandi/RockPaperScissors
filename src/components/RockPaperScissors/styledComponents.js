208563a98c2a1d301be6e7f1982369f6f88f2523
khajapeerbandi <khajapeerbandi@gmail.com>
2023-07-18 14:02:49 +0000

    RockPaperScissors

---
+++ src/components/RockPaperScissors/styledComponents.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div``
+export const ResultContainer = styled.div``
+export const Option = styled.h1``
+export const OptionsContainer = styled.div``
+export const ScoreContainer = styled.div``
+export const ScorePhrase = styled.p``
+export const ScoreNumber = styled.p``
+export const GameViewContainer = styled.div``
+export const GameOptionsList = styled.ul``
+export const TriggerButton = styled.button``
+export const PopupContainer = styled.div``
+export const CloseButton = styled.button``
+export const PopUpImage = styled.img``
+export const PopUpBody = styled.div``
+export const GameResultViewContainer = styled.div``
+export const SelectedOptionsContainer = styled.div``
+export const GameUserOptionContainer = styled.div``
+export const GameParticipantText = styled.p``
+export const GameParticipantChoiceImage = styled.img``
+export const ResultText = styled.p``
+export const PlayAgainButton = styled.button``
